8b6489cd32a9c63ba5f9c0a198001b8c0fa02901
Marcin <gonzur75@gmail.com>
2023-09-28 19:22:44 +0200

    Images model with validators and tests

---
+++ backend/conftest.py
@@ -0,0 +1,39 @@
+import os
+import shutil
+
+import pytest
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+from images.models import Image
+from config import settings
+from django.test import override_settings
+
+
+@pytest.fixture
+def user(db, django_user_model):
+    return django_user_model.objects.create_user(
+        username='testuser',
+        password='testPass123'
+    )
+
+
+@pytest.fixture
+def remove_test_data():
+    yield shutil.rmtree(os.path.join(settings.BASE_DIR, 'test_dir'), ignore_errors=True)
+
+
+@pytest.fixture
+@override_settings(MEDIA_ROOT=os.path.join(settings.BASE_DIR, 'test_dir', 'media'))
+def image_handler(db, user):
+    image_object = Image.objects.create(
+        author=user,
+        url=SimpleUploadedFile('test_file.jpg',
+                               content=open(os.path.join('test_files', 'test_image.jpeg'), 'rb').read())
+    )
+    yield image_object
+
+
+@pytest.fixture
+def image():
+    yield SimpleUploadedFile('test_file.jpg',
+                             content=open(os.path.join('test_files', 'test_image.jpeg'), 'rb').read())
--- backend/images/models.py
+++ backend/images/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+from images.validators import validate_file_type
+
+
 # Create your models here.
+class Image(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    url = models.FileField(upload_to='images/', validators=[validate_file_type])
+
+    created = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.name:
+            self.name = self.url.name
+        return super().save(*args, **kwargs)
